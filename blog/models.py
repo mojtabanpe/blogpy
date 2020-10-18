4d59b3442ce76e0efc15b549cfe6f845a7f2d2e7
mojtabanpe <43112914+mojtabanpe@users.noreply.github.com>
2020-10-18 17:58:52 +0330

    cadd models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,42 @@
-from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+from ckeditor.fields import RichTextField
+
+
+def validate_file_extension(value):
+    import os
+    from django.core.exceptions import ValidationError
+    ext = os.path.splitext(value.name)[1]
+    valid_extensions = ['.jpg', '.png']
+    if not ext.lower() in valid_extensions:
+        raise ValidationError("Unsupported file extension")
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.FileField(upload_to="files/user_avatar/",
+                              blank=False, null=False, validators=[validate_file_extension])
+    description = models.CharField(max_length=512, null=False, blank=False)
+
+    def __str__(self):
+        return self.user.first_name
+
+class Article(models.Model):
+    title = models.CharField(max_length=128, blank=False, null=False)
+    cover = models.FileField(upload_to="files/article_cover/", blank=False, null=False, validators=[validate_file_extension])
+    content = RichTextField()
+    create_at = models.DateTimeField(default=datetime.now)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    author = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    title = models.CharField(max_length=128, blank=False, null=False)
+    cover = models.FileField(upload_to="files/category_cover/", blank=False, null=False, validators=[validate_file_extension])
+
+    def __str__(self):
+        return self.title
--- blogpy/settings.py
+++ blogpy/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
+    'ckeditor'
 ]
 
 MIDDLEWARE = [
@@ -70,17 +69,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'blogpy.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -100,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -114,8 +114,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+# ckeditor config
+CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
+CKEDITOR_UPLOAD_PATH = "uploads/"
