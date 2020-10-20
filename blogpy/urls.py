04774a64c44218c1944801de628cc6312f3fa555
mojtabanpe <43112914+mojtabanpe@users.noreply.github.com>
2020-10-20 13:09:55 +0330

    add static and trmplate

--- blogpy/urls.py
+++ blogpy/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 
+import blog
+
 urlpatterns = [
-    path('', admin.site.urls),
+    # path('', admin.site.urls),
     path('admin/', admin.site.urls),
-    path('ckeditor/', include('ckeditor_uploader.urls'))
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    url(r'^',include('blog.urls'))
 ]
